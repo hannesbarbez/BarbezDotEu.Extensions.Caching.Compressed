dff65e0b35a885127e6f5e6c32f8194d369a3e2e
Hannes Barbez <hannesbarbez@hotmail.com>
2024-12-09 22:47:28 +0100

    AbstractEncapsulatedMemoryCache

---
+++ BarbezDotEu.Extensions.Caching.Compressed/CompressedMemoryCache.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using MessagePack;
+
+namespace BarbezDotEu.Extensions.Caching.Compressed
+{
+    /// <summary>
+    /// Implements an <see cref="IEncapsulatedMemoryCache"/> using <see cref="MessagePackSerializer"/> to compress cached objects.
+    /// </summary>
+    public class CompressedMemoryCache : AbstractEncapsulatedMemoryCache<KeyValuePair<string, byte[]>>, IEncapsulatedMemoryCache
+    {
+        private readonly MessagePackSerializerOptions messagePackOptions = MessagePackSerializerOptions.Standard
+            .WithCompression(MessagePackCompression.Lz4BlockArray);
+
+        /// <summary>
+        /// Constructs an <see cref="EncapsulatedMemoryCache"/>.
+        /// </summary>
+        /// <param name="memoryCache">An instance of <see cref="IMemoryCache"/>.</param>
+        public CompressedMemoryCache(IMemoryCache memoryCache)
+            : base(memoryCache)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Set<TCachable, TCaller>(string method, string differentiator, TCachable value, MemoryCacheEntryOptions options)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                MessagePackSerializer.Serialize(memoryStream, value, messagePackOptions);
+                base.Set<TCachable, TCaller>(method, differentiator, new KeyValuePair<string, byte[]>(
+                    typeof(TCachable).AssemblyQualifiedName,
+                    memoryStream.ToArray()),
+                    options);
+            }
+        }
+
+        /// <inheritdoc/>
+        public TReturn Get<TReturn, TCaller>(string method, string differentiator)
+            where TReturn : class
+        {
+            var parsable = base.Get<TCaller>(method, differentiator);
+            if (parsable is KeyValuePair<string, byte[]> pair)
+            {
+                using (MemoryStream memoryStream = new MemoryStream(pair.Value))
+                {
+                    var valueType = Type.GetType(pair.Key);
+                    return MessagePackSerializer.Deserialize<TReturn>(memoryStream, messagePackOptions);
+                }
+            }
+
+            return default;
+        }
+    }
+}
